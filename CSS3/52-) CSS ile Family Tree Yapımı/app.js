c2f9815c0f8bcc651c0bd8a97de51b238602579a
Oguzcan Ayan <oguzcanayan43@hotmail.com>
2023-09-07 15:14:39 +0300

    UpdatedCssCodesPrototurk

---
+++ CSS3/52-) CSS ile Family Tree Yapımı/app.js
@@ -0,0 +1,21 @@
+
+document.getElementById("range").addEventListener("input", function(){
+    document.querySelector(".container").style.fontSize = this.value + "px";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
